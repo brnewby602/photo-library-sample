013b0cd2adf24b0817f71b16720ee3ffad92fe69
Bryan Newby <brnewby602@gmail.com>
2016-03-28 08:40:52 -0700

    Initial commit for adding react version of photo library app

---
+++ photo-react/apps.js
@@ -0,0 +1,29 @@
+var images = [
+  { 
+    name: 'mountains',
+    src: 'https://images.unsplash.com/uploads/14128434147336bfb286b/e76494ac?crop=entropy&amp;fit=crop&amp;fm=jpg&amp;h=475&amp;ixjsv=2.1.0&amp;ixlib=rb-0.3.5&amp;q=80&amp;w=1375" class="single-photo__fake-image js-fluid-image',
+    description: 'Sweet mountain picture',
+    rating: 4
+  }, 
+  {
+    name: 'pug-yoda',
+    src: 'https://images.unsplash.com/photo-1453227588063-bb302b62f50b?crop=entropy&amp;fit=crop&amp;fm=jpg&amp;h=475&amp;ixjsv=2.1.0&amp;ixlib=rb-0.3.5&amp;q=80&amp;w=725" class="single-photo__fake-image js-fluid-image',
+    description: 'Preparing to fight the dark side!!!',
+    rating: 5
+  }, 
+  {
+    name: 'polar',
+    src:'https://images.unsplash.com/photo-1433769614376-f7231cd16a55?crop=entropy&amp;fit=crop&amp;fm=jpg&amp;h=475&amp;ixjsv=2.1.0&amp;ixlib=rb-0.3.5&amp;q=80&amp;w=1375" class="single-photo__fake-image js-fluid-image',
+    description: 'I beg to differ, this IS my rock.',
+    rating: 5
+  },
+  {    
+    name: 'faces-of-time',
+    src: 'https://images.unsplash.com/photo-1455651512878-0ddbb4c4d0a5?crop=entropy&amp;fit=crop&amp;fm=jpg&amp;h=475&amp;ixjsv=2.1.0&amp;ixlib=rb-0.3.5&amp;q=80&amp;w=1375" class="single-photo__fake-image js-fluid-image',
+    description: 'The faces of time.',
+    rating: 3
+  }
+];
+
+ReactDOM.render(<FishTable fish={fishData}/>, document.getElementById('app'));
+
